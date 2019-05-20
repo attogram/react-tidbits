a0ba36a1ccc5649d45bb1409bfb4dcce47135c55
Attogram Project <attogram@users.noreply.github.com>
2019-05-20 10:22:05 +0200

    react-tidbits v0.0.5 - docs

--- docs/Tidbits.js
+++ docs/Tidbits.js
@@ -1,5 +1,21 @@
 /**
- * React Tidbits - show ever-changing text
+ * React Tidbits - show ever-changing content
+ *
+ *   example:
+ *
+ *   // interval = milliseconds between changes. 1000 milliseconds = 1 second.
+ *   const interval = 2000;
+ *
+ *   // tidbits = an array of message, text strings or JSX
+ *   const tidbits = [
+ *      "Text message",
+ *      (<span><b>JSX</b> message</span>),
+ *   ];
+ *
+ *   <Tidbits
+ *      interval={interval}
+ *      tidbits={tidbits}
+ *   />
  *
  * License: MIT
  * Repository: https://github.com/attogram/react-tidbits
@@ -7,19 +23,15 @@
 
 'use strict';
 
-const TidbitsVersion = '0.0.4';
+const TidbitsVersion = '0.0.5';
 
 class Tidbits extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
             current : 0,
-            interval: this.props.interval
-                ? this.props.interval
-                : 5000,
-            tidbits : this.props.tidbits
-                ? this.props.tidbits
-                : ['404 Tidbits Not Found'],
+            interval: this.props.interval ? this.props.interval : 5000,
+            tidbits : this.props.tidbits ? this.props.tidbits : ['404 Tidbits Not Found'],
         };
         this.tick = this.tick.bind(this);
     }
